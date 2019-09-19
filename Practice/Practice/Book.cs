ac8a19de303b0d5d69205244437b881b5bfb38f4
vmakundi1 <54956524+vmakundi1@users.noreply.github.com>
2019-09-19 11:04:30 -0600

    Created September 19th
    Basic practice on classes and objects
    Beginners

---
+++ Practice/Practice/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Book
+    {
+        //book data type will compose i.e attributes/a series of attributes which define what it is
+        //below are class attributes. we are creating a blueprint of our book/a specification below/ a template
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BASIC BASICS OF CLASSES AND OBJECTS
+            //classes & objects; think that you need to repr a phone, you cant use strings, double etc.
+            //these allow you to create your own data typ to represent something on your data type
+            //a class is a specification for a new data type. 
+            //an object is instance of a class
+            Book book1 = new Book();   //book1 is a variable storing our object
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();   //book1 is a variable storing our object
+            book2.title = "Alfa Ulela";
+            book2.author = "Shaaban Robert";
+            book2.pages = 300;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.pages);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
