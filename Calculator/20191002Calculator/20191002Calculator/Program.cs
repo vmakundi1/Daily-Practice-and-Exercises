394a8f9c2838298687b4813b4b0175be0bc84e05
vmakundi1 <54956524+vmakundi1@users.noreply.github.com>
2019-10-02 23:33:23 -0600

    Basic Calculator
    Basic Calculator with addition, subtraction, multiplication functions. Practice

---
+++ Calculator/20191002Calculator/20191002Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191002Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //basic calculator
+            //Console.Write("Enter a number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(num1 + num2);
+
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op=="-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op=="*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Ivalid operator");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
